6cb9d3b42a48f409415791b0be60ab01c0235376
Daphne van de Laar <daphne.vande.laar@sogeti.com>
2021-08-06 23:54:05 +0200

    Fix, code could not run. Because return null in OnInitializedAsync from main layout.

--- Wegister/WebUI/Services/CustomerService.cs
+++ Wegister/WebUI/Services/CustomerService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Application.Common.Factories.Interfaces;
 using Application.Common.Viewmodels;
 using Application.Customers.Queries.GetCustomersList;
