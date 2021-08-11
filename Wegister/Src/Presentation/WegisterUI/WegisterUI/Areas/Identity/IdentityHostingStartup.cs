cf412358c73654736dc2e809751e31840317c3f0
Daphne van de Laar <daphne.vande.laar@sogeti.com>
2021-08-11 22:00:34 +0200

    latest cleanup

--- Wegister/Src/Presentation/WegisterUI/WegisterUI/Areas/Identity/IdentityHostingStartup.cs
+++ Wegister/Src/Presentation/WegisterUI/WegisterUI/Areas/Identity/IdentityHostingStartup.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using WegisterUI.Data;
+﻿using Microsoft.AspNetCore.Hosting;
 
 [assembly: HostingStartup(typeof(WegisterUI.Areas.Identity.IdentityHostingStartup))]
 namespace WegisterUI.Areas.Identity
