bd342492a02486e7275ea7fb382f41105f09d417
Daphne van de Laar <daphne.vande.laar@sogeti.com>
2023-08-25 09:00:14 +0200

    Made the pagination component, still needs to apply this to the tables.

---
+++ Wegister/Src/Application/Common/Viewmodels/PaginationVm.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Common.Viewmodels
+{
+    public class PaginationVm
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int Take { get; set; }
+        public int Total { get; set; }
+    }
+}
---
+++ Wegister/Src/Presentation/WegisterUI/WegisterUI/Controls/TablePagination.razor.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Application.Common.Viewmodels;
+
+namespace WegisterUI.Controls
+{
+    public partial class TablePagination
+    {
+        public PaginationVm Pagination { get; set; }
+        private int _amountOfPages { get; set; }
+        private string _pagenumber;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Pagination = new PaginationVm()
+            {
+                Page = 1,
+                PageSize = 10,
+                Total = 100
+            };
+            _amountOfPages = Pagination.Total / Pagination.PageSize;
+        }
+        protected void OnClickPage(int pageNumber)
+        {
+            if(pageNumber == -1)
+            {
+                if(Pagination.Page != 1)
+                    Pagination.Page--;
+            }
+            else if(pageNumber == -11)
+            {
+                if(Pagination.Page != 1)
+                    Pagination.Page = 1;
+            }
+            else if (pageNumber == -2)
+            {
+                if(Pagination.Page != _amountOfPages)
+                    Pagination.Page++;
+            }
+            else if (pageNumber == -22)
+            {
+                Pagination.Page = _amountOfPages;
+            }
+            else
+            {
+                Pagination.Page = pageNumber;
+            }
+
+            _pagenumber = pageNumber.ToString();
+        }
+    }
+}
